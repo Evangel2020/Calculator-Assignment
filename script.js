dd961b1599f1e41f092d8ffe994f17c8fff9d1d2
Evangel2020 <66016031+Evangel2020@users.noreply.github.com>
2020-08-19 14:11:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//display message
+alert('Hey You! Are you ready for some conversion?');
+//state the variables and the formula
+var c = prompt('Enter number in degree celsius for conversion');
+var cTofahr = (9/5) * c + 32;
+//show result in web page
+document.write('The result of your conversion is' +' ' + cTofahr);
+//display result to console
+console.log('cTofahr');
+
